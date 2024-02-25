a6481a93286220ea3d7aa5170b694cfd5ff98b68
medinat akinsowon <medinahakinsowon@gmail.com>
2024-02-25 17:30:53 +0100

    media query

--- index.js
+++ index.js
@@ -126,5 +126,32 @@ closeModalSeasoning.addEventListener('click', closeModalWindow6);
 
 
 
+/*--Header-- &--BackToTop-*/
+
+const header = document.querySelector('[data-header]')
+const backTopBtn = document.querySelector('[data-back-top-btn]')
+
+let lastScrollPos = 0;
+
+const hideHeader = function (){
+  const isScrollBottom = lastScrollPos < window.scrollY;
+  if (isScrollBottom){
+    header.classList.add('hide')
+  } else {
+    header.classList.remove('hide')
+  }
+  lastScrollPos = window.scrollY;
+}
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 50){
+    header.classList.add('active')
+    backTopBtn.classList.add('active')
+    hideHeader();
+  } else {
+    header.classList.remove('active')
+    backTopBtn.classList.remove('active')
+  }
+});
 
 
